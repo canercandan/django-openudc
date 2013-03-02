c8a15134c27ac2bd395337740327d7679d6a3929
Caner Candan <candan@info.univ-angers.fr>
2013-03-03 00:48:00 +0100

    * settings.py: indentation + removed useless variables

--- openudc/settings.py
+++ openudc/settings.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-)
+    )
 
 MANAGERS = ADMINS
 
@@ -17,8 +17,8 @@ DATABASES = {
         # 'PASSWORD': '',                  # Not used with sqlite3.
         # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -73,15 +73,15 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     "static",
-)
+    )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'nj7sjy)xdm%(%+uoz87i74-s-1@rmkxph=n28%)2plz0ajq+mv'
@@ -90,8 +90,8 @@ SECRET_KEY = 'nj7sjy)xdm%(%+uoz87i74-s-1@rmkxph=n28%)2plz0ajq+mv'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+    #     'django.template.loaders.eggs.Loader',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -100,11 +100,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     'django.middleware.http.ConditionalGetMiddleware',
     'middleware.MyMiddleware.MyMiddleware',
-)
+    )
 
 ROOT_URLCONF = 'openudc.urls'
 
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     "./templates",
-)
+    )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -126,20 +126,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+        'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
-    # 'polls',
-    # 'pgpauth',
-    # 'testapp',
-    # 'books',
-    # 'email_extras',
-    # 'DjangoEmailPGP',
-    # 'crispy_forms',
+        'django.contrib.admindocs',
     'pks',
     'peers',
     'udid2',
-)
+    )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -152,25 +145,20 @@ LOGGING = {
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
+            }
+        },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
+            }
+        },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
-        },
+            },
+        }
     }
-}
-
-# EMAIL_EXTRAS_USE_GNUPG = True
-# EMAIL_EXTRAS_GNUPG_HOME = '/home/candan/.gnupg'
-
-# CRISPY_TEMPLATE_PACK = "bootstrap"
