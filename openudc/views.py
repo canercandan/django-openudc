56e26ca0c2d11b7d39810f72c954c60f646cced5
Caner Candan <candan@info.univ-angers.fr>
2013-03-03 10:49:55 +0100

    * udid2: fixed an issue

--- openudc/views.py
+++ openudc/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 # from models import *
 
 from udid2 import Udid2
-__keys__ = [x for x in Udid2.HashedVerboseParser()()]
+__keys__ = [x for x in Udid2.ClearVerboseParser()()]
 
 def home(request):
     return render(request, 'home.html', {'keys': __keys__,})
--- peers/views.py
+++ peers/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from models import Peer
 
 from udid2 import Udid2
-__keys__ = [x for x in Udid2.HashedVerboseParser()()]
+__keys__ = [x for x in Udid2.ClearVerboseParser()()]
 
 def list(request):
     return render(request, 'peers/list.html', {'form': Peer(), 'keys': __keys__,})
