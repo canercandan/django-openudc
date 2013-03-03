08c45b43ca89eb26e7470c86a048b1c66ff6047e
Caner Candan <candan@info.univ-angers.fr>
2013-03-03 10:49:26 +0100

    * openudc/urls.py: removed useless comments

--- openudc/urls.py
+++ openudc/urls.py
@@ -5,24 +5,14 @@ from django.views.generic import DetailView, ListView, TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-from udid2 import Udid2
-__keys__ = [x for x in Udid2.HashedVerboseParser()()]
-
 urlpatterns = patterns('',
-                       # Examples
-                       url(r'^$', 'openudc.views.home', name='home'),
-                       url(r'^capabilities$', 'openudc.views.capabilities', name='capabilities'),
-
-                       # url(r'^openudc/', include('openudc.foo.urls')),
-
                        # Uncomment the admin/doc line below to enable admin documentation
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
                        # Uncomment the next line to enable the admin
                        url(r'^admin/', include(admin.site.urls)),
-                       # url(r'^polls/', include('polls.urls')),
-                       # url(r'^pgpauth/', include('pgpauth.urls')),
-                       # url(r'^books/', include('books.urls')),
+
+                       url(r'^$', 'openudc.views.home', name='home'),
+                       url(r'^capabilities$', 'openudc.views.capabilities', name='capabilities'),
                        url(r'^pks/', include('pks.urls')),
                        url(r'^peers/', include('peers.urls')),
                        )
